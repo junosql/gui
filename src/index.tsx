a31948d329e5a31f8ddb641bf336f8aefb40d167
Wallace Nascimento Pereira <wallace.pereira@semantix.com.br>
2021-01-19 20:32:47 -0300

    feat: setup project main files

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <div> Hello JunoSQL </div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('#main'));
